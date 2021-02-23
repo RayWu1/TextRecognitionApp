17b0dd1acbfbf7ad0a88801ee505360c8f4949f4
raywu1 <rayiziers@gmail.com>
2021-02-23 12:34:08 +0000

    firebase

--- app/src/main/java/com/example/textrecognitionapp/MainActivity.java
+++ app/src/main/java/com/example/textrecognitionapp/MainActivity.java
@@ -38,14 +38,14 @@ public class MainActivity extends AppCompatActivity
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            // display error state to the user
+
+        If (takePictureIntent.resolveActivity(getPackageManager(>)
         }
-    }
+}
+
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
